5d21b8d2ca47cb708c76f3abe04c12fe9dc6a1da
MerveALKAN <17701037@mersin.edu.tr>
2021-03-28 22:01:56 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,71 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+
+            _cars = new List<Car> {
+            new Car { Id = 1, BrandId = 1, ColorId = 1, DailyPrice = 200, ModelYear=2018, Description="Mercedes" },
+            new Car { Id = 2, BrandId = 2, ColorId = 2, DailyPrice = 100, ModelYear = 2017, Description = "Fiat" },
+            new Car { Id = 3, BrandId = 2, ColorId = 2, DailyPrice = 250, ModelYear = 2015, Description = "Bmw" },
+            new Car { Id = 4, BrandId = 1, ColorId = 1, DailyPrice = 100, ModelYear = 2017, Description = "Peugeot" },
+            new Car { Id = 5, BrandId = 3, ColorId = 1, DailyPrice = 170, ModelYear = 2016, Description = "Audi" }
+
+            };
+    }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == c.Id);
+            _cars.Remove(carToDelete);
+
+
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetByCategory(int Id)
+        {
+            return _cars.Where(c => c.Id == Id).ToList();
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(c => c.Id == Id).ToList();
+        }
+
+        public void GetById(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == c.Id);
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
