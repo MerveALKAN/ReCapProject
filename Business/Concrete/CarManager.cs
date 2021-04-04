c748e0f3a9e9faef5a6bfeaf82fde85bdf4485a6
MerveALKAN <17701037@mersin.edu.tr>
2021-04-04 14:30:32 +0300

    04.04.2021

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -39,7 +39,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Car>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
             }
